043bb4aa97818b4c29d87e30267c734887cffa16
Ema3035 <92973981+Ema3035@users.noreply.github.com>
2021-11-03 12:10:49 +0100

    Add files via upload

---
+++ c++/while/ciclo while.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int numero = 0;
+   while (numero<=199){
+       numero=numero+ 1;
+       cout<<" "<<numero;
+    }
+    return 0;
+}
