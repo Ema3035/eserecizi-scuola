cfb6d5969cfcb6cc9a5c14073898ae87ed53accf
Ema3035 <92973981+Ema3035@users.noreply.github.com>
2022-10-04 11:16:57 +0200

    Add files via upload

---
+++ Java/04_10_2022/Es2/Es2.java
@@ -0,0 +1,12 @@
+
+public class Es2 {
+    
+    public static void main(String[] args){
+        int n = 10;
+        int[] vettore= new int[n];
+        for (int i = 0; i<n; i++){
+            vettore[i]= i;
+            System.out.println(vettore[i]);
+        }
+    }
+}
