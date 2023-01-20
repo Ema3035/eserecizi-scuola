1f4afef69613cb1493d3348de594f718d30bda2b
Emanuele <92973981+Ema3035@users.noreply.github.com>
2023-01-20 17:13:33 +0100

    Update Sacchetto.java

--- Java/Classe Tombola/Sacchetto.java
+++ Java/Classe Tombola/Sacchetto.java
@@ -5,7 +5,7 @@ class Sacchetto {
     private int indice;
 
     public Sacchetto() {
-        // inizializzazione dell'array di numeri
+
         numeri = new int[90];
         for (int i = 0; i < 90; i++) {
             numeri[i] = i + 1;
@@ -13,16 +13,16 @@ class Sacchetto {
         indice = numeri.length - 1;
     }
 
-    // metodo per estrarre un numero casuale dal sacchetto
+
     public int estraiNumero() {
         if (indice < 0) {
             return -1;
         }
-        // utilizzare una classe Random per generare un indice casuale
+
         Random rand = new Random();
         int randomIndex = rand.nextInt(indice + 1);
 
-        // scambiare il numero estratto con l'ultimo numero nel sacchetto
+
         int estratto = numeri[randomIndex];
         numeri[randomIndex] = numeri[indice];
         numeri[indice] = estratto;
