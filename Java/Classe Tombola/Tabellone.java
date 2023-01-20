985430bd912c9781655ab7e5cd47b079b69e7955
Emanuele <92973981+Ema3035@users.noreply.github.com>
2023-01-20 17:13:59 +0100

    Update Tabellone.java

--- Java/Classe Tombola/Tabellone.java
+++ Java/Classe Tombola/Tabellone.java
@@ -5,18 +5,16 @@ class Tabellone {
 
     public Tabellone(Giocatore[] giocatori) {
         this.giocatori = giocatori;
-        // inizializzazione della matrice di numeri per il tabellone
+
         numeri = new int[9][10];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 10; j++) {
                 numeri[i][j] = -1;
             }
         }
-        // inizializzazione del sacchetto
         sacchetto = new Sacchetto();
     }
 
-    // metodo per estrarre un numero dal sacchetto e annunciarlo ai giocatori
     public int estraiNumero() {
         int numero = sacchetto.estraiNumero();
         System.out.println("Estratto il numero: " + numero);
@@ -26,7 +24,7 @@ class Tabellone {
                 System.out.println(giocatori[i].getNome() + " ha coperto il numero " + numero + " sulla sua cartella!");
             }
         }
-        // inserire il numero estratto nel tabellone
+        
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 10; j++) {
                 if (numeri[i][j] == -1) {
