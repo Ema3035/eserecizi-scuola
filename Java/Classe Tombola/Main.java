7c9173c618cddd96ff88e56d5fb173d8553feac4
Emanuele <92973981+Ema3035@users.noreply.github.com>
2023-01-20 17:14:50 +0100

    Update Main.java

--- Java/Classe Tombola/Main.java
+++ Java/Classe Tombola/Main.java
@@ -1,17 +1,17 @@
 public class Main{
     
 public static void main(String[] args) {
-    // creazione dei giocatori
+
     Giocatore giocatore1 = new Giocatore("Emanuele");
     Giocatore giocatore2 = new Giocatore("Marco");
     Giocatore[] giocatori = {giocatore1, giocatore2};
-    // riempimento delle cartelle dei giocatori
+
     for (int i = 0; i < giocatori.length; i++) {
         giocatori[i].riempiCarte();
     }
-    // creazione della tombola
+
     Tombola tombola = new Tombola(giocatori);
-    // inizio del gioco
+
     tombola.gioca();
 }
 }
